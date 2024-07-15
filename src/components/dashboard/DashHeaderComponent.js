640acedde84cc937b5fe156bc10cd20284af903e
techiemokhele <neomokhele23@gmail.com>
2024-07-15 14:16:58 +0200

    Implement footer section

--- src/components/dashboard/DashHeaderComponent.js
+++ src/components/dashboard/DashHeaderComponent.js
@@ -1,4 +1,7 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import { MdCarRepair } from "react-icons/md";
 import { FaBell } from "react-icons/fa";
 import { CiLogout } from "react-icons/ci";
@@ -12,30 +15,53 @@ const links = {
 };
 
 const DashHeaderComponent = () => {
+    const navigate = useNavigate()
+    const { pathname } = useLocation()
+
+    const onGoHomeClicked = () => navigate('/dash')
+
+    let goHomeButton = null
+    if (pathname !== '/dash') {
+        goHomeButton = (
+            <button
+                className="dash-footer__button icon-button"
+                title="Home"
+                onClick={onGoHomeClicked}
+            >
+                <FontAwesomeIcon icon={faHouse} />
+            </button>
+        )
+    }
+
     return (
-        <header className='flex flex-row justify-between w-full p-3 bg-gray-950'>
-            <div className='flex flex-row justify-center items-start w-[22%]'>
-                <Link to="/dash" className="text-md flex flex-row gap-1 items-center">
+        <header className='flex flex-row justify-between w-full py-3 bg-gray-950 px-6'>
+            <div className='flex flex-row justify-start items-start w-[25%]'>
+                <Link to="/" className="text-md flex flex-row gap-1 items-center">
                     <MdCarRepair size={24} className='text-orange-500' />
                     <h1 className='text-[12px]'>Neo M. Auto Repairs</h1>
                 </Link>
             </div>
 
-            <div className='flex flex-row justify-center items-start w-[70%]'>
-                <nav className="dash-header__nav">
-                    <div className='flex flex-row'>
-                        {links.company.map((link) => (
-                            <Link key={link.href} href={link.href}>
-                                <span className="px-3 py-2 rounded-md text-sm font-normal text-white hover:text-orange-500 cursor-pointer ">
-                                    {link.label}
-                                </span>
-                            </Link>
-                        ))}
-                    </div>
-                </nav>
+            <div className='flex flex-row justify-center items-center w-[50%]'>
+                <div className='flex flex-row'>
+                    {goHomeButton && (
+                        <Link href="/dash">
+                            <span className="px-3 py-2 rounded-md text-sm font-normal text-white hover:text-orange-500 cursor-pointer ">
+                                Home
+                            </span>
+                        </Link>
+                    )}
+                    {links.company.map((link) => (
+                        <Link key={link.href} href={link.href}>
+                            <span className="px-3 py-2 rounded-md text-sm font-normal text-white hover:text-orange-500 cursor-pointer ">
+                                {link.label}
+                            </span>
+                        </Link>
+                    ))}
+                </div>
             </div>
 
-            <div className='flex flex-row justify-end items-center w-[10%] gap-2'>
+            <div className='flex flex-row justify-end items-center w-[5%] gap-2'>
                 <FaBell size={20} className='text-white cursor-pointer hover:text-orange-500' />
                 <CiLogout size={20} className='text-white cursor-pointer hover:text-orange-500' />
             </div>
