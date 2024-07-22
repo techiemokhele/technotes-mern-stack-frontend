2b77128e3d6a81fb260cea3603423786a8962909
techiemokhele <neomokhele23@gmail.com>
2024-07-22 18:11:58 +0200

    Implement NewNote update

--- src/features/notes/NewNote.js
+++ src/features/notes/NewNote.js
@@ -1,11 +1,14 @@
 import { useSelector } from 'react-redux'
+
 import { selectAllUsers } from '../users/usersApiSlice'
 import NewNoteForm from './NewNoteForm'
+
+import LoadingContentComponent from '../../components/constant/LoadingContentComponent'
 
 const NewNote = () => {
     const users = useSelector(selectAllUsers)
 
-    const content = users ? <NewNoteForm users={users} /> : <p>Loading...</p>
+    const content = users ? <NewNoteForm users={users} /> : <LoadingContentComponent />;
 
     return content
 }
