a79f1bf0a6e3cb1e7dfedf568b5ad76658aa2551
techiemokhele <neomokhele23@gmail.com>
2024-08-04 23:26:02 +0200

    Implement devtools disable

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
 import App from './App'
 import { store } from './app/store'
 import './index.css'
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
